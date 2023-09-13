1fd54ba9a80f465094e1b93e8b92124459e46973
dlsdk0601 <dlsdk0601@gmail.com>
2023-09-14 00:48:18 +0900

    feat: null 과 const vs final

--- lib/main.dart
+++ lib/main.dart
@@ -1,66 +1,6 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  // n/a nullable
-  // 변수 타입에 ?를 붙여준다
-  String name = "good";
-  String? name2 = "red";
-
-  // 불가능
-  // name = null;
-  // 가능
-  // name2 = null;
-
-  print(name);
-  print(name2);
-
-  // n/n non-nullable
-  // ! 도 있는데 이건 절대 null 이 아니라는걸 암시한다.
-  // 위에서 name2 가 null 이라고 했기 때문에 에러가 난다.
-  // 위에서 name2가 string 타입이면 경고가 뜬다. ! 가 의미 없다고
-  print(name2!);
-
-  // DateTime => 시간 데이터 타입
-  DateTime now = DateTime.now();
-  print(now); // 현재 날짜와 시간이 찍힌다.
-  DateTime now2 = DateTime.now();
-
-  // 해당 시간이 찍히는 시간은 해석기가 해당 코드를 읽을 때다.
-  // 앱이 실행될때 딱 찍히는게 아니다. => now != now2
-
-  // final => 이뮤터블 하게 바껴서 변경 불가능한 상수
-  final String name3 = "good";
-  print(name3);
-  // 불가능
-  // name3 = "red"
-
-  // const => 역시나 상수라는걸 의미해서 변경 불가능
-  const String name4 = "black";
-  print(name4);
-
-  // 불가
-  // name4 = "white";
-
-  /*
-  * 그럼 final 과 const 의 차이는 뭘까?
-  * final 과 const 의 공통 특징은 데이터 타입을 명시 하지 않아도 된다.
-  * 그럴꺼 같은게, 어차피 상수 니까 한번 선언 되면 수정 할 수 없다.
-    final name5 = "string";
-     const name6 = "string"; => 둘다 가능
-  *
-  * 그럼 차이는 뭐냐
-  * final 은 빌드 타임을 몰라도 되지만, const 는 빌드 타임을 알아야한다.
-  * const DateTime now2 = DateTime.now();
-  * 를 하기 되면 에러가 난다.
-  *
-  * 그러면 빌드 타입이 뭐냐
-  * => 빌드가 될때의 시간이다.
-  * 즉, const 로 선언하면 에러가 나는 이유는 DateTime.now 는 코드가 실행되야 알수 있기 때문에
-  * const 로 선언하면 에러가 난다.
-  *
-  * 쉽게 말해서 당장 알 수 있는 값들만 const 로 가능
-  * */
-
   // runApp(const MyApp());
 }
 
