7bf6769df45bc8583ca813ed43b50ecc5356e18a
dlsdk0601 <dlsdk0601@gmail.com>
2023-09-19 00:09:13 +0900

    feat: typedef

--- lib/main.dart
+++ lib/main.dart
@@ -1,177 +1,38 @@
-import 'package:flutter/material.dart';
-
 void main() {
-  addNumbers(10, 20, 31);
+  // Operation 의 타입을 가지는 add 함수를 operation 에 저장 시키는 것.
+  // 즉 operation 은 add 에서 정의한 함수가 들어간다.
+  Operation operation = add;
 
-  // named parameter 는 순서 상관없이 key ~ value 로 넘긴다.
-  addNumbers1(x: 10, y: 20, z: 30);
+  int result = operation(10, 20, 30);
+
+  print(result);
+
+  operation = subtract;
+
+  int result2 = operation(30, 20, 10);
+
+  print(result2);
+
+  int result3 = calculate(30, 40, 50, add);
+
+  print(result3);
+
+  int result4 = calculate(30, 40, 50, subtract);
+
+  print(result4);
 }
 
-// 세개의 숫자 x, y, z 를 더하고 홀 짝 판별 함수
-// y에 ? 를 붙이면 null 로 처리 된다.
-// z 는 기본값을 넣은것이다
-// 해당 함수는 파라미터의 순서가 중요하다.
-addNumbers(int x, [int z = 10, int? y]) {
-  int sum = x + z;
+// typedef
+// 함수의 타라미터와 return 타입을 정의한 type
+typedef Operation = int Function(int x, int y, int z);
 
-  if (sum % 2 == 0) {
-    print("짝수");
-  } else {
-    print("홀수");
-  }
-}
+// 더하기 함수
+int add(int x, int y, int z) => z + y + z;
 
-// named parameter - 이름이 있는 파라미터 => 순서가 중요하지 않게 된다.
-// required 를 빼면 옵셔널로 된다.
-// void - 뜻: 공허
-// 함수 앞에는 return 의 타입을 정의한다.
-// void 는 return 이 없는 함수를 의미한다.
-void addNumbers1({required int x, required int y, int z = 30}) {
-  int sum = x + y + z;
+// 빼기 함수
+int subtract(int x, int y, int z) => z - y - z;
 
-  if (sum % 2 == 0) {
-    print("짝수");
-  } else {
-    print("홀수");
-  }
-}
-
-// named 와 positional 파라미터 두개 동시에 가
-void addNumbers2(int x, {required int y, int z = 30}) {
-  int sum = x + y + z;
-
-  if (sum % 2 == 0) {
-    print("짝수");
-  } else {
-    print("홀수");
-  }
-}
-
-// 총합을 return
-int addNumbers3(int x, {required int y, int z = 30}) {
-  int sum = x + y + z;
-  return sum;
-}
-
-// arrow function
-int addNumbers4(int x, {required int y, int z = 30}) => x + y + z;
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+// 계산
+int calculate(int x, int y, int z, Operation operation) {
+  return operation(x, y, z);
 }
