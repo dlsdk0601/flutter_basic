3906cfda9cff1d5f60c0f4bfc580bdda4b23af1f
dlsdk0601 <dlsdk0601@gmail.com>
2023-09-19 21:26:23 +0900

    feat: const oop

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,34 @@
 void main() {
-  // flutter 에서도 new 키워드를 안넣어도 된다.
-  // Idol blackPink = Idol();
+  // 생성자에 const 로 선언한다.
   Idol blackPink = Idol("BlackPink", ["A", "B", "C", "D"]);
 
+  // 이렇게 변수를 바꿀 수도 있는데, 못바꾸게 하는게 안전하고 좋다.
+  // class 안에 변수에 final 을 넣어둔다.
+  // blackPink.name = "changed";
   print(blackPink.name);
   blackPink.sayHello();
 
-  Idol bts = Idol("BTS", ["E", "F", "G"]);
-  print(bts.name);
-  bts.sayHello();
+  Idol blackPink2 = Idol("BlackPink", ["A", "B", "C", "D"]);
+  // 사람이 봤을때는 blackPink 와 같다고 본다. 대입한 값이 같으니까,
+  // 근데 코드에서는 두개가 다르다.
 
-  Idol test = Idol.fromList([
-    ["E", "F", "G"],
-    "test"
-  ]); // 이렇게 생성자를 생성해도 된다.
-  // 생성자를 두개 만들 수 있다.
+  print(blackPink == blackPink2); // false
+
+  // 근데 웃긴건 const 로 인스턴스를 만들경우 같다고 나온다.
+  Idol blackPink3 = const Idol("BlackPink", ["A", "B", "C", "D"]);
+  Idol blackPink4 = const Idol("BlackPink", ["A", "B", "C", "D"]);
+
+  print(blackPink3 == blackPink4); // true
 }
 
-// 아이돌 클래스
 class Idol {
-  // 먼저 정의 하는 방법
-  // String name = "블랙핑크";
-  // List<String> memebers = ["A", "B", "C", "D"];
+  // final 로 선언하게 되면 인스턴스 밖에서 수정이 불가능
+  final String name;
+  final List<String> memebers;
 
-  // 생성자에서 파라미터를 받아서 대입할 변수
-  String name;
-  List<String> memebers;
+  // const 를 붙이면 인스턴스를 만들때도 앞에 const 를 붙일수 있다.
+  const Idol(this.name, this.memebers);
 
-  // contructor (positional)
-  // Idol(String name, List<String> members)
-  //     : this.name = name,
-  //       this.memebers = members;
-
-  // 생성자를 이렇게 간단하게 넣을 수 있다. 순서만 맞다면 파라미터를 바로 대입한다.
-  Idol(this.name, this.memebers);
-
-  // named contructor
   Idol.fromList(List values)
       : this.memebers = values[0],
         this.name = values[1];
