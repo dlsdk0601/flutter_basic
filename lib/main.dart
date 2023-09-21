a38d4fcab0e01f81fa691f77e8bac8cbe76994da
inajung <ina.jung@coint.co.kr>
2023-09-21 15:34:54 +0900

    feat: list.map

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,23 @@
 void main() {
-  List<String> blackPink = ["A", "B", "C", "C"];
+  List<String> blackPink = ["A", "B", "C"];
+
+  // js 처럼 새로운 리스트를 return 한다.
+  final newBlackPink = blackPink.map((x) {
+    return 'test $x';
+  }).toList();
 
   print(blackPink);
+  print(newBlackPink); // toList() 를 하지 않으면, Iterable 형태로 return 하게 된다.
 
-  // map 으로 형 변환
-  print(blackPink.asMap());
-  print(blackPink.toSet());
+  final newBlackPink2 = blackPink.map((e) => 'test $e');
+  print(newBlackPink2.toList());
 
-  Map blackPinkMap = blackPink.asMap();
+  print(blackPink == blackPink); // true
+  print(blackPink == newBlackPink); // false
+  print(newBlackPink == newBlackPink2); // false
+  // => 원소의 값은 같아도, map 은 새로운 리스트를 반환하기에 == 가 성립하지 않음
 
-  print(blackPinkMap.keys);
-  // (0, 1, 2, 3) => 이런 소괄호는 Iterable, 이걸 List 로 바꾸려면 .toList() 하면됨
-  print(blackPinkMap.values); // (A, B, C, C)
-
-  Set blackPinkSet = Set.from(blackPink);
-  print(blackPinkSet);
-
-  // list, set, map 은 서로간에 형변환이 자유롭게 가능
+  String number = '13579'; // [1.jpg, 2.jpg ... ] 로 수정
+  final parsed = number.split("").map((e) => '$e.jpg').toList();
+  print(parsed);
 }
