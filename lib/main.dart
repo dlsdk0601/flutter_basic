073108f0db67c03ab789bd475445a5b4a90f65f5
inajung <ina.jung@coint.co.kr>
2023-09-21 15:49:33 +0900

    feat: Set.map

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,6 @@
 void main() {
-  Map<String, String> test = {"A": "A", "B": "B", "C": "C"};
+  Set blackPinkSet = {"A", "B", "C", "D"};
 
-  // Map 을 map 돌리면 파라미터를 두개를 받는다 (key, value)
-  // Map 으로 새로운 Map 만들때
-  final result = test.map((key, value) => MapEntry("test $key", "TEST $value"));
-  print(result);
-  print(test);
-
-  // Map 의 key, value 따로 구분 지어서 작업 할때
-  final keys = test.keys.map((e) => "Test $e").toList();
-  final values = test.values.map((e) => "Test $e").toList();
-  print(keys);
+  final newSet = blackPinkSet.map((e) => "test $e").toList();
+  print(newSet);
 }
