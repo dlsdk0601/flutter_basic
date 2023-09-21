5da1bfe9616ea444dda74af79d688ace60c2d828
inajung <ina.jung@coint.co.kr>
2023-09-21 14:32:24 +0900

    feat: generic

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,20 @@
 void main() {
-  BoyGroup bts = BoyGroup("BTS");
-  GirlGroup newJeans = GirlGroup("newJeans");
-  // IdolInterface test = IdolInterface("test"); 에러 난다.
+  Lecture<String, String> lecture1 = Lecture("123", "Lecture1");
+  lecture1.printIdType();
 
-  bts.sayName();
-  newJeans.sayName();
-
-  print(bts is IdolInterface); // true
-  print(bts is BoyGroup); // true
-  print(bts is GirlGroup); // false
+  Lecture<int, String> lecture2 = Lecture(123, "lecture2");
+  lecture2.printIdType();
 }
 
-// interface
-// class 를 만들때 해당 interface 의 형태를 같게 하기 위해 사용
-// 필수적으로 넣어야하는 변수나 method 를 정의 할 수 있다.
-// abstact 를 넣으면 추상화가 되어서 해당 interface 로 instance 를 만들지 못한다.
-// IdolInterface 로 instance 를 만들려는 실수를 방지
-abstract class IdolInterface {
-  String name;
+// generic - 타입을 외부에서 받아와서 사용
+// ts 처럼 <> 안에 타입 사용 가능
+class Lecture<T, X> {
+  final T id;
+  final X name;
 
-  IdolInterface(this.name);
+  Lecture(this.id, this.name);
 
-  // abstract 를 사용하면 함수의 body 를 적지 않아도된다.
-  void sayName();
-}
-
-class BoyGroup implements IdolInterface {
-  String name;
-
-  BoyGroup(this.name);
-
-  void sayName() {
-    print("$name");
-  }
-}
-
-class GirlGroup implements IdolInterface {
-  String name;
-
-  GirlGroup(this.name);
-
-  void sayName() {
-    print("$name");
+  void printIdType() {
+    print(id.runtimeType);
   }
 }
