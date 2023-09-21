c7659ca6dc046161cc454ef1eed09a8df2efefbb
inajung <ina.jung@coint.co.kr>
2023-09-21 18:42:41 +0900

    feat: stream ~ yield

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,33 @@
-import 'dart:async';
-
 void main() async {
-  final controller = StreamController();
-  // final stream = controller.stream; 하나의 리스너 함수를 위해
-  final stream = controller.stream.asBroadcastStream(); // 여러개의 리스너 함수를 위해
-
-  // where 로 stream 에 들어오는 값에 따라 커스텀 가능
-  final streamListener1 = stream.where((event) => event % 2 == 0).listen((val) {
-    print("Listener 1: $val");
+  // 현재는 이렇게 하면 1 과 2가 같이 하나씩 실행된다.
+  calculate(1).listen((event) {
+    // print(event); // 1을 넣었으니, 1 * i 를 계속 뱉어낸다.
   });
 
-  final streamListener2 = stream.where((event) => event % 2 == 1).listen((val) {
-    print("Listener 2: $val");
+  calculate(2).listen((event) {
+    // print(event);
   });
 
-  controller.sink.add(1);
-  controller.sink.add(2);
-  controller.sink.add(3);
-  controller.sink.add(4);
+  playAllStream().listen((event) {
+    print(event);
+  });
+}
+
+Stream<int> playAllStream() async* {
+  yield* calculate(1); // 이렇게 작성하면 1 의 모든값을 다 가져올때 까지 기다린다.
+  yield* calculate(100); // 1 의 값들이 다 출력되면 100이 실행되서 출력한다.
+}
+
+Stream<int> calculate(int number) async* {
+  for (int i = 0; i < 5; i++) {
+    // 이렇게 하게 되면 i=0 일때만 가능하다. return 되면 함수는 끝나기 때문
+    // return i * number;
+
+    // loop 가 다 돌면서 값을 다 받고 싶으면, 함수 타입을 Stream 으로 바꿔준다.
+    // async 에 * 를 붙여준다.
+    // return 대신에 yield 를 해준다.
+    yield i * number;
+
+    await Future.delayed(Duration(seconds: 1)); // 1 초 마다 값을 뱉는다.
+  }
 }
