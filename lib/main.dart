3fb69aabb9ce56604b7e4ca6b4989f5783c19b64
inajung <ina.jung@coint.co.kr>
2023-09-21 15:48:30 +0900

    feat: Map.map

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,14 @@
 void main() {
-  List<String> blackPink = ["A", "B", "C"];
+  Map<String, String> test = {"A": "A", "B": "B", "C": "C"};
 
-  // js 처럼 새로운 리스트를 return 한다.
-  final newBlackPink = blackPink.map((x) {
-    return 'test $x';
-  }).toList();
+  // Map 을 map 돌리면 파라미터를 두개를 받는다 (key, value)
+  // Map 으로 새로운 Map 만들때
+  final result = test.map((key, value) => MapEntry("test $key", "TEST $value"));
+  print(result);
+  print(test);
 
-  print(blackPink);
-  print(newBlackPink); // toList() 를 하지 않으면, Iterable 형태로 return 하게 된다.
-
-  final newBlackPink2 = blackPink.map((e) => 'test $e');
-  print(newBlackPink2.toList());
-
-  print(blackPink == blackPink); // true
-  print(blackPink == newBlackPink); // false
-  print(newBlackPink == newBlackPink2); // false
-  // => 원소의 값은 같아도, map 은 새로운 리스트를 반환하기에 == 가 성립하지 않음
-
-  String number = '13579'; // [1.jpg, 2.jpg ... ] 로 수정
-  final parsed = number.split("").map((e) => '$e.jpg').toList();
-  print(parsed);
+  // Map 의 key, value 따로 구분 지어서 작업 할때
+  final keys = test.keys.map((e) => "Test $e").toList();
+  final values = test.values.map((e) => "Test $e").toList();
+  print(keys);
 }
