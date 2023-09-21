4fb5cc4d9f87405976d5c4a2143d94c68dcacb55
inajung <ina.jung@coint.co.kr>
2023-09-21 18:29:12 +0900

    feat: basic stream

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,21 @@
+import 'dart:async';
+
 void main() async {
-  // Future - 미래
-  // 미래에 받아올 값
-  Future<String> name = Future.value("test");
-  Future<int> number = Future.value(1);
-  Future<bool> isTrue = Future.value(true);
+  final controller = StreamController();
+  // final stream = controller.stream; 하나의 리스너 함수를 위해
+  final stream = controller.stream.asBroadcastStream(); // 여러개의 리스너 함수를 위해
 
-  // delayed - 지연되다. => js 의 setTimeOut 과 같다.
-  // 1 - 지연할 시간
-  // 2 - 지연 시간 지난후 실행함 콜백
-  Future.delayed(Duration(seconds: 2), () {
-    // print("delay");
+  // where 로 stream 에 들어오는 값에 따라 커스텀 가능
+  final streamListener1 = stream.where((event) => event % 2 == 0).listen((val) {
+    print("Listener 1: $val");
   });
 
-  final result1 = await addNumbers(1, 1);
-  final result2 = await addNumbers(2, 2);
-
-  print("result1: $result1");
-  print("result2: $result2");
-  print("result1 + result2 = ${result1 + result2}");
-}
-
-// 원래 void 인데, js 에서 promise 처럼 해주려면 Future 로 감싼다.
-Future<int> addNumbers(int num1, int num2) async {
-  print("계산 시작: $num1 + $num2");
-
-  // async - await 를 사용하면 비동기를 동기처럼 사용 가능
-  await Future.delayed(Duration(seconds: 2), () {
-    print("계산완료: ${num1 + num2}");
+  final streamListener2 = stream.where((event) => event % 2 == 1).listen((val) {
+    print("Listener 2: $val");
   });
 
-  print("함수 완료");
-  return num1 + num2;
+  controller.sink.add(1);
+  controller.sink.add(2);
+  controller.sink.add(3);
+  controller.sink.add(4);
 }
