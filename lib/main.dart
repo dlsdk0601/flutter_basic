635e81b176372ae1dae149631da3e76f89f73cfd
inajung <ina.jung@coint.co.kr>
2023-09-21 15:55:55 +0900

    feat: list where

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,17 @@
 void main() {
-  Set blackPinkSet = {"A", "B", "C", "D"};
+  List<Map<String, String>> people = [
+    {"name": "A", "group": "blackPink"},
+    {"name": "B", "group": "BTS"},
+    {"name": "C", "group": "BTS"},
+    {"name": "D", "group": "blackPink"}
+  ];
 
-  final newSet = blackPinkSet.map((e) => "test $e").toList();
-  print(newSet);
+  print(people);
+
+  // return 값이 true 이면 그냥 넘기고, false 이면 해당 list 에서 삭제 시킨다.
+  // js 에서 filter 와 같음
+  final result = people.where((element) => element['group'] == "blackPink");
+  final result1 = people.where((element) => element['group'] == "BTS");
+  print(result);
+  print(result1);
 }
