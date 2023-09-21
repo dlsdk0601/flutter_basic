cdd381ac8521f20b94a66cb50597caf8524c7542
inajung <ina.jung@coint.co.kr>
2023-09-21 17:52:08 +0900

    feat: async - await (Future)

--- lib/main.dart
+++ lib/main.dart
@@ -1,52 +1,34 @@
-void main() {
-  List<int> even = [2, 4, 6, 8];
-  List<int> odd = [1, 3, 5, 7];
+void main() async {
+  // Future - 미래
+  // 미래에 받아올 값
+  Future<String> name = Future.value("test");
+  Future<int> number = Future.value(1);
+  Future<bool> isTrue = Future.value(true);
 
-  // cascading operator
-  // js 에서 복사와 같다.
-  print([...even, ...odd]);
-  print(even == [...even]); // false (복사도 완전 다른 배열을 반환한다.)
+  // delayed - 지연되다. => js 의 setTimeOut 과 같다.
+  // 1 - 지연할 시간
+  // 2 - 지연 시간 지난후 실행함 콜백
+  Future.delayed(Duration(seconds: 2), () {
+    // print("delay");
+  });
 
-  // 리스트 와 Map 은 자유도가 너무 높다. string 에 오타가 나도 모른다.
-  // 그래서 class 를 사용해서 구조화해서 다루는게 안전하다.
-  final List<Map<String, String>> people = [
-    {"name": "A", "group": "blackPink"},
-    {"name": "B", "group": "BTS"},
-    {"name": "C", "group": "BTS"},
-    {"name": "D", "group": "blackPink"}
-  ];
+  final result1 = await addNumbers(1, 1);
+  final result2 = await addNumbers(2, 2);
 
-  // 당장에 에러가 나는 이유는 name 과 group 이 무조건 있을거라는 보장이 없어서 이다.
-  // 그래서 이 값이 무조건 있다는 ! 를 붙여서 에러를 없애준다.
-  final parsedPeople =
-      people.map((x) => Person(name: x["name"]!, group: x["group"]!)).toList();
-  print(parsedPeople);
-
-  // class 형태로 바꾸고 난 후
-  for (Person person in parsedPeople) {
-    // 자동 완성 이 편해짐
-    print(person.group);
-  }
-
-  final bts = parsedPeople.where((x) => x.group == "BTS").toList();
-  print(bts);
-
-  // 이렇게 한번에 연쇄적으로 로직 가능
-  final result = people
-      .map((e) => Person(name: e["name"]!, group: e["group"]!))
-      .where((element) => element.group == "BTS");
-  print(result);
+  print("result1: $result1");
+  print("result2: $result2");
+  print("result1 + result2 = ${result1 + result2}");
 }
 
-class Person {
-  final String name;
-  final String group;
+// 원래 void 인데, js 에서 promise 처럼 해주려면 Future 로 감싼다.
+Future<int> addNumbers(int num1, int num2) async {
+  print("계산 시작: $num1 + $num2");
 
-  Person({required this.name, required this.group});
+  // async - await 를 사용하면 비동기를 동기처럼 사용 가능
+  await Future.delayed(Duration(seconds: 2), () {
+    print("계산완료: ${num1 + num2}");
+  });
 
-  @override
-  String toString() {
-    // print(parsedPeople) 의 출력 값을 볼때 출력되는 문자열이 바뀜
-    return "Person(name: $name, group: $group)";
-  }
+  print("함수 완료");
+  return num1 + num2;
 }
