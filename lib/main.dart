593ba631e52ba7179214f2f0731c9db5190a9ad8
dlsdk0601 <dlsdk0601@gmail.com>
2023-09-23 20:37:29 +0900

    feat: record

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,57 @@
 void main() async {
-  // 현재는 이렇게 하면 1 과 2가 같이 하나씩 실행된다.
-  calculate(1).listen((event) {
-    // print(event); // 1을 넣었으니, 1 * i 를 계속 뱉어낸다.
-  });
+  final result = nameAndAge({'name': 'test', 'age': 20});
 
-  calculate(2).listen((event) {
-    // print(event);
-  });
+  print(result);
+  print(result.$1); // String 타입 (반환값의 첫번쨰) 를 기준으로 내장함수를 부른다.
+  print(result.$2);
 
-  playAllStream().listen((event) {
-    print(event);
-  });
-}
+  final result2 = getNewJeans();
 
-Stream<int> playAllStream() async* {
-  yield* calculate(1); // 이렇게 작성하면 1 의 모든값을 다 가져올때 까지 기다린다.
-  yield* calculate(100); // 1 의 값들이 다 출력되면 100이 실행되서 출력한다.
-}
-
-Stream<int> calculate(int number) async* {
-  for (int i = 0; i < 5; i++) {
-    // 이렇게 하게 되면 i=0 일때만 가능하다. return 되면 함수는 끝나기 때문
-    // return i * number;
-
-    // loop 가 다 돌면서 값을 다 받고 싶으면, 함수 타입을 Stream 으로 바꿔준다.
-    // async 에 * 를 붙여준다.
-    // return 대신에 yield 를 해준다.
-    yield i * number;
-
-    await Future.delayed(Duration(seconds: 1)); // 1 초 마다 값을 뱉는다.
+  for (final item in result2) {
+    print(item.$1);
+    print(item.$2);
   }
+
+  final result3 = getNewJeansWithType();
+
+  for (final item in result3) {
+    print(item.$1);
+    print(item.$2);
+  }
+
+  final result4 = getNewJeansWithType2();
+
+  for (final item in result4) {
+    print(item.name);
+    print(item.age);
+  }
+}
+
+// record
+(String, int) nameAndAge(Map<String, dynamic> json) {
+  return (json['name'] as String, json['age'] as int);
+}
+
+// 원래라면 주석 처럼 처리해야하는데, 새로운 문법으로 가능
+// List<Map<String, dynamic>> getNewJeans() {
+List<(String, int)> getNewJeans() {
+  return [
+    ("민지", 20),
+    ("혜린", 18),
+  ];
+}
+
+// 이렇게 타입에 이름을 줄수 있다.
+List<(String name, int age)> getNewJeansWithType() {
+  return [
+    ("민지", 20),
+    ("혜린", 18),
+  ];
+}
+
+List<({String name, int age})> getNewJeansWithType2() {
+  return [
+    (name: "민지", age: 20),
+    (name: "혜린", age: 18),
+  ];
 }
